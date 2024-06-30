25fde753d86e2ba95e9ff7711e6861ff24351f40
Lars van Rhijn <larsvanrhijn@gmail.com>
2024-06-30 08:05:01 +0200

    Make notes attribute optional

--- website/uphance/clients/models/contact.py
+++ website/uphance/clients/models/contact.py
@@ -1,10 +1,12 @@
-from mode_groothandel.clients.utils import get_value_or_error
+from typing import Optional
+
+from mode_groothandel.clients.utils import get_value_or_error, get_value_or_none
 
 
 class Contact:
 
     def __init__(
-        self, first_name: str, last_name: str, position: str, phone_1: str, phone_2: str, email: str, notes: str
+        self, first_name: str, last_name: str, position: str, phone_1: str, phone_2: str, email: str, notes: Optional[str]
     ):
         self.first_name = first_name
         self.last_name = last_name
@@ -23,5 +25,5 @@ class Contact:
             phone_1=str(get_value_or_error(data, "phone_1")),
             phone_2=str(get_value_or_error(data, "phone_2")),
             email=str(get_value_or_error(data, "email")),
-            notes=str(get_value_or_error(data, "notes")),
+            notes=get_value_or_none(data, "notes", str),
         )
