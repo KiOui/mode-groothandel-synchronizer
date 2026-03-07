ba74757d0e64b9ca1700d7379144a2bfb8ff60bf
Lars van Rhijn <larsvanrhijn@gmail.com>
2026-03-07 08:20:15 +0100

    Run the actual Celery task instead of a 'fake' one

--- website/credit_notes/tasks.py
+++ website/credit_notes/tasks.py
@@ -12,9 +12,6 @@ from celery import shared_task
 def synchronize_credit_notes():
     """Synchronize all credit notes."""
     first_credit_note = CreditNote.objects.all().order_by("uphance_id").first()
-    CreditNote.objects.create(uphance_id=first_credit_note.uphance_id+1)
-    return
-    first_credit_note = CreditNote.objects.all().order_by("uphance_id").first()
     if first_credit_note is not None:
         synchronize_credit_notes_from_id = first_credit_note.uphance_id
     else:
